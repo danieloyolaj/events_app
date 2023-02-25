cc8c9ce563c9cff585323db6ee65203aad538a50
Daniel Oyola <danieloyola@softerdo.com>
2023-02-25 00:13:34 -0400

    First commit

---
+++ src/pages/events/index.js
@@ -0,0 +1,30 @@
+const Page = () => {
+  return (
+    <>
+    <div>
+      <h1>Events page</h1>
+      <div>
+      <a href="">
+            <img />
+            <h2>Events in London</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </a>
+          <a href="">
+            <img />
+            <h2>Events in Cobija</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </a>
+          <a href="">
+            <img />
+            <h2>Events in Santa Cruz</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </a>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Page
+
+//We create an index.js page in each folder we want to route
